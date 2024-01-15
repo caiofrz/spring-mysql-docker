eed054727924ed085e75f08518b3bdc14d43534a
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-14 21:33:14 -0300

    initial commit

---
+++ src/test/java/br/com/springmysqldockertest/SpringMysqlDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springmysqldockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
