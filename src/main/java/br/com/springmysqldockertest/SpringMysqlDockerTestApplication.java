89b8b76148481ad1ff813af8955b7d2c9d72000d
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-01-15 15:26:06 -0300

    add initial classes and mysql dependencies

--- src/main/java/br/com/springmysqldockertest/SpringMysqlDockerTestApplication.java
+++ src/main/java/br/com/springmysqldockertest/SpringMysqlDockerTestApplication.java
@@ -1,13 +1,21 @@
 package br.com.springmysqldockertest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringMysqlDockerTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMysqlDockerTestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringMysqlDockerTestApplication.class, args);
+  }
 
+  @GetMapping("/")
+  public String index() {
+    return "Container docker funcionando!";
+  }
 }
---
+++ src/main/java/br/com/springmysqldockertest/Users.java
@@ -0,0 +1,21 @@
+package br.com.springmysqldockertest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_userss")
+public class Users {
+
+  @Id
+  private Long id;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
